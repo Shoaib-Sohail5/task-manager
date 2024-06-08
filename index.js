a6bc01f83913610a91706a376f2a552969257a0e
Shoaib Sohail <shoaibsohail.shaik@gmail.com>
2024-06-08 14:41:32 +0530

    Added backend

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import types from './types'
-
 const express = require('express')
-const app = express()
+const { createTodo, updateTodo } = require('./types')
 
+const app = express()
 app.use(express.json())
 
 app.get('/todos', function(req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "Please enter correct inputs"
+        })
+        return;
+    }
     
 })
 
@@ -14,7 +22,14 @@ app.post('/todo', function(req, res) {
 })
 
 app.put('/completed', function(req, res) {
+    const updatePayload = req.body
+    const parsedPayload = updateTodo.safeParse(updatePayload)
 
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "Please enter correct id"
+        })
+    }
 })
 
 
