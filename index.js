8727ffb8f8ab1f0a25b1ef28a4f713cc3badf78e
Shoaib Sohail <shoaibsohail.shaik@gmail.com>
2024-06-08 14:34:22 +0530

    type validation and server is built

---
+++ index.js
@@ -0,0 +1,22 @@
+import types from './types'
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/todos', function(req, res) {
+    
+})
+
+app.post('/todo', function(req, res) {
+
+})
+
+app.put('/completed', function(req, res) {
+
+})
+
+
+
+
